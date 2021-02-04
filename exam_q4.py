f288d91784ceaa930d920caeaa553de9ca776a4e
Chittadeepan <42205302+Chittadeepan@users.noreply.github.com>
2021-02-04 13:26:17 +0530

    Add files via upload

--- exam_q4.py
+++ exam_q4.py
@@ -21,11 +21,18 @@ def main():
     y_0=2
     y_n=45
     h=0.1
-    z_1=-9.8
-    z_2=-5
+    z_1=20
+    z_2=40
     #calling shooting method to solve given boundary value problem
     
     shooting_method(t_0,y_0,h,t_n,y_n,z_1,z_2,f_1,f_2)
-    print("\nThe launch velocity determination depends on guess of slope at initial and final positions which can be calculated by hand")
+    print("\nThe launch velocity is detemined to from initial and final slope through calculation by hand to be around 33 m/s.")
 main()
-    
+'''
+#output
+From the first guess z_1= 20 the corresponding value of y is obtained as -23.032500000000034  at t= 0.
+
+From the second guess z_2= 40 the corresponding value of y is obtained as 78.96750000000009  at t= 5.
+
+The launch velocity determination depends on guess of slope at initial and final positions.
+'''
